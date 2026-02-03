e904d0f63d23974b247a3199577b83b141fd8400
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2026-02-04 02:08:15 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "resource_group_name" {
+  description = "Name of the Azure Resource Group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "virtual_network_name" {
+  description = "Name of the Virtual Network"
+  type        = string
+}
+
+variable "vnet_address_space" {
+  description = "Address space for the Virtual Network"
+  type        = list(string)
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "subnet_address_space" {
+  description = "Address space for the subnet"
+  type        = list(string)
+}
