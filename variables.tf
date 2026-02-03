6d8df5f80cdec0b65427274fc24db7db69d5f2b3
rajeshshambudu <168664690+rajeshshambudu@users.noreply.github.com>
2026-02-04 02:19:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,29 +1,32 @@
 variable "resource_group_name" {
-  description = "Name of the Azure Resource Group"
-  type        = string
+  type = string
 }
 
 variable "location" {
-  description = "Azure region"
-  type        = string
+  type = string
 }
 
 variable "virtual_network_name" {
-  description = "Name of the Virtual Network"
-  type        = string
+  type = string
 }
 
 variable "vnet_address_space" {
-  description = "Address space for the Virtual Network"
-  type        = list(string)
+  type = list(string)
 }
 
 variable "subnet_name" {
-  description = "Name of the subnet"
-  type        = string
+  type = string
 }
 
 variable "subnet_address_space" {
-  description = "Address space for the subnet"
-  type        = list(string)
+  type = list(string)
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
 }
